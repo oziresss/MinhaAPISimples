5356af122cd6b5feeeb9fe93e948a4e98eaaac0f
Ozires Alves Da Rocha Filho <ozires.filho@gpssa.com.br>
2023-03-07 21:39:14 -0300

    Adicionar arquivos de projeto.

---
+++ MinhaAPISimples/Model/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinhaAPISimples.Model
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
